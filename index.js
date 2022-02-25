2dc9ed84fdc5ff26b0cc37a1497218fcd95fa044
manishgenius11 <98729507+manishgenius11@users.noreply.github.com>
2022-02-25 19:50:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+let div = document.getElementById("videodiv");
 
+
+// (1) Show/Display Videos on Homepage
+
+
+
+async function display(){
+
+    // q = Popular Videos --> Popular%20Videos (%20 -> Space)
+
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyDfvD6Lp-LAoo_bikSnkrDJ0pP2EZarkYs`);
+
+    let data = await res.json();
+
+    for({id:{videoId}}of data.items)
+    {
+    let videodiv= document.createElement("iframe")
+    videodiv.setAttribute("class","mons")
+    videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+    videodiv.allow = 'fullscreen'
+    div.append(videodiv)
+    }
+
+}
+
+display();
+
+
+
+
+// (2) Search When Something is typed in the search-box
+
+async function searchVideos(){
+    document.getElementById("videodiv").innerHTML=""; // Empty
+
+    let query = document.getElementById("video").value; // Search String/ Query String
+
+//search for videos
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyDfvD6Lp-LAoo_bikSnkrDJ0pP2EZarkYs`);
+let data = await res.json();
+
+for({id:{videoId}}of data.items)
+{
+let videodiv= document.createElement("iframe")
+videodiv.setAttribute("class","mons")
+videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+videodiv.allow = 'fullscreen'
+div.append(videodiv)
+}
+
+}
